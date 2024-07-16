4ea63458cc450d1bf3b3c8ba0e4c42278ae1576b
AkinOlu21 <kenneth.akinola@outlook.com>
2024-07-16 17:45:56 +0100

    added functionality for listing orders for admin panel

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -92,4 +92,16 @@ const userOrders = async (req,res) => {
     }
 } 
 
-export {placeOrder,verifyOrder,userOrders}
+//Listing orders for the admin panel
+const listOrders = async (req,res) =>{
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"error"})
+        
+    }
+}
+
+export {placeOrder,verifyOrder,userOrders,listOrders}
