ec8c6505e6ab1e734a2bd7472adf63a02f6f39a0
AkinOlu21 <kenneth.akinola@outlook.com>
2024-07-31 10:30:49 +0100

    Created pharmacy model

---
+++ models/pharmacyModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const pharmacySchema = new mongoose.Schema({
+    name: {type:String, rewquired:true},
+    address:{type:String, required:true},
+    phoneNumber:{type:String, required:true},
+    email:{type:String, required:true, unique:true},
+    pharmaNumber:{type:String, required:true, unique:true},
+    operatingHours:{ Monday:{open:String, close:String}, Tuesday:{open:String, close:String}, Wednesday:{open:String, close:String}, Thursday:{open:String, close:String},
+     Friday:{open:String, close:String}, Saturday:{open:String, close:String}, Sunday:{open:String, close:String}}
+}, {timestamps:true});
+
+const pharmacyModel = mongoose.models.pharmacy || mongoose.moodel("Pharmacy",pharmacySchema);
+
+export default pharmacyModel;
